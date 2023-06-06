0eaf29c102bbe072aa7852f4e46b6f1f151bfd24
thisuserhasnoname1 <87253545+thisuserhasnoname1@users.noreply.github.com>
2023-06-06 15:50:09 +1000

    Moved Hash and Salt to Security
    Moved Hash and Salt to Security. 'sec' Security object located in AppFunctions

--- src/AppFunctions.java
+++ src/AppFunctions.java
@@ -1,10 +1,6 @@
 import java.sql.*;
 import javax.swing.*;
 import java.util.ArrayList;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.time.LocalDateTime;
 
 public class AppFunctions {
@@ -14,6 +10,7 @@ public class AppFunctions {
     Statement stmt;
     ResultSet rs;
     String databaseName = "glitchtracking"; // local database file name
+    Security sec = new Security(); // used for all security functions
 
     // ----------------------------------------------------------------------------------------------------
     // CONSTRUCTOR
@@ -195,58 +192,6 @@ public class AppFunctions {
     }
 
     /**
-     * Generate a SHA-256 hash of the input string
-     * 
-     * Credit to ChatGPT for this method
-     * 
-     * @param input
-     * @return
-     */
-    public static String generateSHA256Hash(String input) {
-        try {
-            // Create MessageDigest instance for SHA-256
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
-            // Apply SHA-256 hash to the input string
-            byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
-
-            // Convert the byte array to a hexadecimal string
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : encodedHash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
-            // Handle exception if SHA-256 algorithm is not available
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Generate a random 4 character salt to be used for password hashing
-     * 
-     * @return 4 character string
-     */
-    public static String generateSalt() {
-        String SALT_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        SecureRandom random = new SecureRandom();
-        StringBuilder salt = new StringBuilder();
-
-        for (int i = 0; i < 4; i++) {
-            int randomIndex = random.nextInt(SALT_CHARACTERS.length());
-            char randomChar = SALT_CHARACTERS.charAt(randomIndex);
-            salt.append(randomChar);
-        }
-
-        return salt.toString();
-    }
-
-    /**
      * Populate staff table with dummy data
      */
     public void populateStaff() {
@@ -272,9 +217,9 @@ public class AppFunctions {
                 firstDotLast = firstName[i].toLowerCase() + "." + lastName[i].toLowerCase();
                 email = firstDotLast + "@gmail.com";
                 password = firstDotLast + state[i] + postcode[i];
-                passhash = generateSHA256Hash(password);
-                rfid = generateSHA256Hash(passhash);
-                salt = generateSalt();
+                passhash = sec.generateSHA256Hash(password);
+                rfid = sec.generateSHA256Hash(passhash);
+                salt = sec.generateSalt();
                 dateTime = LocalDateTime.now();
 
                 String query = "INSERT INTO staff (firstname, lastname, phone, email, street, suburb, state, postcode, passhash, salt, rfid, lastlogin) "
--- src/Security.java
+++ src/Security.java
@@ -1,6 +1,63 @@
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class Security {
     public static void main(String[] args) {
+        System.out.println("Hello Security World");
 
+    }
+
+    /**
+     * Generate a SHA-256 hash of the input string
+     * 
+     * @param input
+     * @return
+     */
+    public String generateSHA256Hash(String input) {
+        try {
+            // Create MessageDigest instance for SHA-256
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // Apply SHA-256 hash to the input string
+            byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Handle exception if SHA-256 algorithm is not available
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Generate a random 4 character salt to be used for password hashing
+     * 
+     * @return 4 character string
+     */
+    public String generateSalt() {
+        String SALT_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder salt = new StringBuilder();
+
+        for (int i = 0; i < 4; i++) {
+            int randomIndex = random.nextInt(SALT_CHARACTERS.length());
+            char randomChar = SALT_CHARACTERS.charAt(randomIndex);
+            salt.append(randomChar);
+        }
+
+        return salt.toString();
     }
 
     public Security() {
